3f7bb34fe43baedc399972c59a240b41f74d92ae
HuihuaChen <qangela314@gmail.com>
2017-08-01 19:55:38 +0800

    timeline

---
+++ js/index.js
@@ -0,0 +1,49 @@
+var vm = new Vue({
+  el: "#app",
+  data:{
+    logs: [ ]
+  },
+  methods:{
+    initial(){
+      this.logs=[];
+      this.logs=[
+  {
+    year: 2014,
+    content:
+    [
+      {
+        month: 7,
+        tag: "咖啡廳副店長的日子",
+      }
+    ]
+   },
+   {
+    year: 2015,
+    content:
+    [
+      {
+       tag: "小淘花之北醫藝術季參展+擺市集" ,
+      }
+    ]
+   },{
+    year: 2016,
+    content:[
+      {
+        tag: "瘋狂補學分+醫院實習吃便當",
+      }
+    ]
+   },{
+    year: 2017,
+    content:
+    [{
+      tag:"踏入python與網頁設計坑",
+    }
+    ]
+   },
+ ]
+
+
+    }
+  }
+  
+});
